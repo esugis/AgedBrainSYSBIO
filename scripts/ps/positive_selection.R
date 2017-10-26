7ff46b3de5a20893364af0a9256ed9875b54d719
nikolaeva <elenanik@ut.ee>
2017-10-26 15:38:15 +0300

    path fix in ps scripts

--- scripts/ps/positive_selection.R
+++ scripts/ps/positive_selection.R
@@ -1,10 +1,14 @@
 # This script expracts ensg name, score from positiv selection dataset.
 
-# Set working directory
-setwd("~/absb/results/ps/")
+# Create the folder where current results will be written
+resdir<-paste("~/AgedBrainSYSBIO/results","ps",sep="/")
+dir.create(file.path(resdir),showWarnings = FALSE, recursive = TRUE)
+
+# Set created directory as working dirrectory
+setwd(resdir)
 
 # Load the data
-ps <- read.csv(file = "~/absb/data/ps/positive_selection_data.csv", sep  = ",", header = T, stringsAsFactors = F)
+ps <- read.csv(file = "~/AgedBrainSYSBIO/data/ps/positive_selection_data.csv", sep  = ",", header = T, stringsAsFactors = F)
 
 # Extract the columns containing ENSG id and p-value
 ps <- ps[,c(1,8)]
