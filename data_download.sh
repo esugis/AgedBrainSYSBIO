58684037f20d2db268c783ad200a5b7f50a8340c
nikolaeva <elenanik@ut.ee>
2017-12-05 19:54:56 +0200

    bash script for automatic data download

---
+++ data_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://ndownloader.figshare.com/files/9918205?private_link=65859545441cbc5b70ac -O epistasis/ADNI_CT_epistasis.txt
+wget https://ndownloader.figshare.com/files/9918220?private_link=65b137e4198c1849177f -O epistasis/ADNI_VER_epistasis.tsv
+wget https://ndownloader.figshare.com/files/9918214?private_link=65da7ebf68a1938b4df0 -O epistasis/HBTRC_epistasis.tsv
+wget https://ndownloader.figshare.com/files/9918217?private_link=687b22e229f32f6aa302 -O epistasis/TGEN_epistasis.tsv
---
+++ scripts/libraries.R
@@ -0,0 +1,3 @@
+# This script installs required packages
+
+# 
