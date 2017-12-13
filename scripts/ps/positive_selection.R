a83b7c84303ef5c2a60ce47f9575c4bbc322958b
nikolaeva <elenanik@ut.ee>
2017-12-13 19:04:33 +0200

    positive selection- significant selected. 14 genes

--- scripts/ps/positive_selection.R
+++ scripts/ps/positive_selection.R
@@ -1,23 +1,21 @@
 # This script expracts ensg name, score from positiv selection dataset.
 
 # Create the folder where current results will be written
-resdir<-paste("~/AgedBrainSYSBIO/results","ps",sep="/")
+resdir<-paste("~/absb/results","ps",sep="/")
 dir.create(file.path(resdir),showWarnings = FALSE, recursive = TRUE)
 
 # Set created directory as working dirrectory
 setwd(resdir)
 
 # Load the data
-ps <- read.csv(file = "~/AgedBrainSYSBIO/data/ps/positive_selection_data.csv", sep  = ",", header = T, stringsAsFactors = F)
+ps <- read.csv(file = "~/absb/data/ps/positive_selection_data.csv", sep  = ",", header = T, stringsAsFactors = F)
 
 # Extract the columns containing ENSG id and p-value
 ps <- ps[,c(1,8)]
+ps <- na.omit(ps[ps$FDR.corrected.Pval<=0.05,])
 
 # Rename columns
 colnames(ps) <- c("ensg", "ps_pval")
-
-# Remove NAs
-ps <- na.omit(ps)
 
 # Save the data
 save(ps, file = "ps.RData")
